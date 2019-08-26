ccc95073a3867a6061d26c318c55893d3e619ba8
lorence-crowdbotics <jlorencelim@gmail.com>
2019-08-26 04:37:09 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -1,26 +1,40 @@
 import unittest
 import re
 
-class TestAcceptanceStripe(unittest.TestCase):
+
+class TestAssessment(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        super(TestAcceptanceStripe, self).__init__(*args, **kwargs)
+        super(TestAssessment, self).__init__(*args, **kwargs)
         with open('order.html', 'r') as file_descriptor:
             self.dom_str = file_descriptor.read()
 
-    #Check if redirectToCheckout function call is present        
-    def test_acceptance_redirect_to_checkout(self):
-        return True
-        # self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
+    # Check if redirectToCheckout function call is present
+    def test_assesssment_redirect_to_checkout(self):
+        self.assertNotEqual(self.dom_str, '.redirectToCheckout',
+                            'No stripe redirect call found!')
+
+    # Check if successUrl redirects to order_success.html
+    def test_assesssment_success_url(self):
+        self.assertRegex(self.dom_str,
+                         r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'',
+                         'No order_success.html redirect found on checkout success.')
+
+    # Check if cancelUrl redirects to order.html
+    def test_assesssment_cancel_url(self):
+        self.assertRegex(self.dom_str,
+                         r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'',
+                         'No order.html redirect found on checkout cancel.')
+
+
+ 
+
+class TestSubmission(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSubmission, self).__init__(*args, **kwargs)
+        with open('order.html', 'r') as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
     
-    #Check if successUrl redirects to order_success.html
-    def test_acceptance_success_url(self):
-        return True
-        # self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
-    
-    #Check if cancelUrl redirects to order.html
-    def test_acceptance_cancel_url(self):
-        return True
-        # self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
         
 if __name__ == '__main__':
     unittest.main()
